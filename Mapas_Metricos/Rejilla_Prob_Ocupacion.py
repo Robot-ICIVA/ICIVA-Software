1986770cbf840ffae0f9de93cc988a5a5afc087f
Eladio <eladiorjb@gmail.com>
2018-07-01 10:08:44 -0430

    Mapa listo

--- Mapas_Metricos/Rejilla_Prob_Ocupacion.py
+++ Mapas_Metricos/Rejilla_Prob_Ocupacion.py
@@ -2,6 +2,9 @@ import numpy as np
 import pygame as pg
 import matplotlib.pyplot as plt
 from settings import *
+import math
+import scipy
+from scipy.stats import norm
 vec = pg.math.Vector2
 
 class Ball():
@@ -18,6 +21,13 @@ class Grid():
     def __init__(self):
         self.aliveNodes = []
         self.centroide = []
+        self.P_Malas = []
+        I_x = 0
+        I_y = 0
+        self.ICIVA = None #vec(I_x, I_y)
+        E_x = 1
+        E_y = 1
+        self.ENEMIGO = None #vec(E_x, E_y)
         #self.connections = [vec(1, 0), vec(-1, 0), vec(0, 1), vec(0, -1), vec(1, 1), vec(-1, 1), vec(-1, -1),vec(1, -1)]
 
     def vec2int(self, vec):
@@ -47,17 +57,26 @@ class Visualize():
     def new(self):
         # Se presenta el Mapa
         self.playing = True
+        self.estado = "neutro"
         self.Grid = Grid()
+        self.matrix = np.zeros((int((Largo_C*32)/(TILESIZE)),int((Ancho_C*32)/(TILESIZE))))
+        self.Prob = np.zeros((int((Largo_C * 32) / (TILESIZE)), int((Ancho_C * 32) / (TILESIZE))))
+        #print(self.matrix)
+        #print(np.size(self.matrix,0))
         self.Ball = Ball()
-        self.Balls = []
+        #self.Balls = []
+
         Coord_X_m = 0.96
         Coord_Y_m = 0.96
         Coord_X_cm = Coord_X_m*100
         Coord_Y_cm = Coord_Y_m*100
-        Coord_X = int(Coord_X_cm/(Resolucion))-1
-        Coord_Y = -int(Coord_Y_cm/(Resolucion))+1
+        Coord_X = int(Coord_X_cm/(Resolucion))#-1
+        Coord_Y = -int(Coord_Y_cm/(Resolucion))#+1
+
         self.Grid.aliveNodes = []#[vec(Coord_X, Coord_Y)] # [vec(0, 0), vec(0, 1), vec(0, -1)]
         self.Grid.centroide = []#[vec(Coord_X, Coord_Y),vec(Coord_X+20, Coord_Y+10)]
+        self.Grid.P_Malas = []
+
     def run(self):
         # Lazo de Visualizacion
         while self.playing:
@@ -72,50 +91,201 @@ class Visualize():
             if event.type == pg.QUIT:
                 if self.playing == True:
                     self.playing = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_b: #Pelotas buenas
+                    self.estado = "P_Buenas"
+                elif event.key == pg.K_m: #Pelotas malas
+                    self.estado = "P_Malas"
+                elif event.key == pg.K_i: #Vehiculo ICIVA
+                    self.estado = "ICIVA"
+                elif event.key == pg.K_e: #Vehiculo Enemigo
+                    self.estado = "ENEMIGO"
+
             if event.type == pg.MOUSEBUTTONDOWN:
-                mouse_pos = (vec(pg.mouse.get_pos()) - vec(0, largo - 1 * TILESIZE)) // TILESIZE  # getting the tile coordinates of the mouse
-
+                mouse_pos = (vec(pg.mouse.get_pos()) - vec(0, 0)) // TILESIZE  # getting the tile coordinates of the mouse
+                #mouse_pos = (vec(pg.mouse.get_pos()) - vec(0, largo - 1 * TILESIZE)) // TILESIZE
                 # spawning or destroying a cell
-                if (mouse_pos in self.Grid.centroide):# or (mouse_pos in self.Ball.relleno):
-                    self.Grid.centroide.remove(mouse_pos)
-                    #self.Grid.aliveNodes.remove(mouse_pos)
-                    #self.Ball.centro.remove(mouse_pos)
-                    #self.Ball.relleno = []
-                else:
-                    self.Grid.centroide.append(mouse_pos)
-                    #self.Ball.centro.append(mouse_pos)
 
+                if self.estado == "P_Buenas":
+                    if (mouse_pos in self.Grid.centroide):# or (mouse_pos in self.Ball.relleno):
+                        self.Grid.centroide.remove(mouse_pos)
+                        #self.Grid.aliveNodes.remove(mouse_pos)
+                        #self.Ball.centro.remove(mouse_pos)
+                        #self.Ball.relleno = []
+                    else:
+                        self.Grid.centroide.append(mouse_pos)
+                    #print(int(mouse_pos.x))
+                    #print(int(-mouse_pos.y))
+
+                    #print(self.matrix[int(-mouse_pos.y)][int(mouse_pos.x)])
+                    #self.matrix.itemset((int(-mouse_pos.y), int(mouse_pos.x)), 1)
+                    #print(self.matrix[int(-mouse_pos.y)][int(mouse_pos.x)])
+
+                    ##self.Ball.centro.append(mouse_pos)
+
+                elif self.estado == "P_Malas" or self.estado == "ENEMIGO":
+
+                    if (mouse_pos in self.Grid.P_Malas):
+                        self.Grid.P_Malas.remove(mouse_pos)
+                    else:
+                        self.Grid.P_Malas.append(mouse_pos)
+
+                    if self.estado == "ENEMIGO":
+                        if (mouse_pos == self.Grid.ENEMIGO):
+                            self.Grid.ENEMIGO = None
+
+                        else:
+                            if self.Grid.ENEMIGO in self.Grid.P_Malas:
+                                self.Grid.P_Malas.remove(self.Grid.ENEMIGO)
+                            self.Grid.ENEMIGO = mouse_pos
+
+
+                elif self.estado == "ICIVA":
+                    if mouse_pos == self.Grid.ICIVA:
+                        self.Grid.ICIVA = None
+                    else:
+                        self.Grid.ICIVA = mouse_pos
+
+    def Distance(self, centroide, x, y):
+        X = int(centroide.x)
+        Y = int(centroide.y)
+        Xo = int(x)
+        Yo = int(y)
+        Distancia = math.sqrt((X-Xo)**2 + (Y-Yo)**2)
+        return Distancia
+
+    def Ocupacion(self, Grid):
+
+        Dist = np.zeros((int((Largo_C*32)/(TILESIZE)),int((Ancho_C*32)/(TILESIZE))))
+
+        #Calculo de matriz de posiciones
+        yn = np.arange((int((Largo_C * 32) / (TILESIZE))))
+        xn = np.arange((int((Ancho_C * 32) / (TILESIZE))))
+        matrix_coord = np.meshgrid(yn, xn)
+        index_matrix_coord = np.dstack((matrix_coord[0], matrix_coord[1]))
+
+        # Centrar
+        sigma = 0
+
+        # Escala
+        escala = 10
+        alpha = 4 * escala
+        pqc = 10 * escala
+
+        for centro in Grid.centroide:
+            Dif = index_matrix_coord - np.array((centro.y, centro.x))
+            Dist = np.linalg.norm(Dif, axis= 2)
+            M = pqc * scipy.stats.norm(sigma, alpha).pdf(Dist)
+            self.Prob = self.Prob + M
+            ind = self.Prob > 1
+            self.Prob[ind] = 1
+
+        for centro in Grid.P_Malas:
+            Dif = index_matrix_coord - np.array((centro.y, centro.x))
+            Dist = np.linalg.norm(Dif, axis= 2)
+            M = pqc * scipy.stats.norm(sigma, alpha).pdf(Dist)
+            self.Prob = self.Prob - M
+            ind = self.Prob < -1
+            self.Prob[ind] = -1
+
+        if (len(Grid.centroide) == 0 and len(Grid.P_Malas) == 0):
+            div = 1
+        else:
+            # print(type(self.Prob))
+            max = np.ndarray.max(abs(self.Prob))
+            # print(max)
+            div = max
+        self.Prob = self.Prob / div
+
+        """
+        for centro in Grid.P_Malas:
+            Dif = index_matrix_coord - np.array((centro.y, centro.x))
+            Dist = np.linalg.norm(Dif, axis= 2)
+            self.Prob = self.Prob - pqc * scipy.stats.norm(sigma, alpha).pdf(Dist)
+        """
+
+        """
+        if(len(Grid.centroide) == 0 and len(Grid.P_Malas) == 0):
+            div=1
+        else:
+            #print(type(self.Prob))
+            max = np.ndarray.max(abs(self.Prob))
+            #print(max)
+            div = max
+        self.Prob= self.Prob/div
+        """
+
+        print("Prob 2: ")
+        print(self.Prob)
+
+
+
+    def PintarProb(self, screen):
+        #Se pintan los cuadritos con probabilidad en la matriz con distinta intensidad
+
+        #""""
+        #print("Pintar: ")
+        #print(self.Prob)
+        self.Prob = np.flip(self.Prob, 1)
+        #L= largo
+        for i in range(self.Prob.shape[0]):
+            #L = largo-1*TILESIZE
+            for j in range(self.Prob.shape[1]):
+                rect = pg.Rect(((i * TILESIZE), (-j * TILESIZE)+ largo-1*TILESIZE), (TILESIZE, TILESIZE))
+                #print("X = "+str(i)+" Y = "+str(j))
+                G = abs(int(self.Prob[i][j]*255))
+                #print(G)
+                COLOR = (G, G, G)
+                #print(COLOR)
+                pg.draw.rect(screen, COLOR, rect)
+        self.Prob = np.zeros((int((Largo_C * 32) / (TILESIZE)), int((Ancho_C * 32) / (TILESIZE))))
+        #"""
+        #pass
     def update(self):
         self.Grid.update()
 
     def drawing(self):
         # Dibujo de la Grid
         self.ventana.fill(DARKGREY)
-        self._draw_ball_(self.Grid, self.Ball)
+        self._draw_ball_(self.Grid, self.Ball, self.matrix)
+        self.Ocupacion(self.Grid)
+        self.PintarProb(self.ventana)
         self._draw_nodes(self.Grid, self.ventana)
         self._draw_grid()
         self._draw_grid1()
+
 
         # self.draw_grid()
         pg.display.flip()
 
     def _draw_nodes(self, Grid, screen):
+        """
         for node in Grid.aliveNodes:
-            #print("_draw_nodes:"+str(node))
-            #rect = pg.Rect(((node.x * TILESIZE) + ancho // 2, (node.y * TILESIZE) + largo // 2), (TILESIZE, TILESIZE))
-            rect = pg.Rect(((node.x * TILESIZE), (node.y * TILESIZE) + largo-1*TILESIZE), (TILESIZE, TILESIZE))
+            rect = pg.Rect(((node.x * TILESIZE), (node.y * TILESIZE)), (TILESIZE, TILESIZE))
             pg.draw.rect(screen, GREY, rect)
-            #rect1 = pg.Rect(((42 * TILESIZE), (-100 * TILESIZE + largo - 1 * TILESIZE)), (TILESIZE, TILESIZE))
-            #pg.draw.rect(screen, RED, rect1)
-        #pg.draw.circle(screen, RED, (int(ancho/2), int(largo/2)), int(C_cuadros/2)* TILESIZE, 3)
+        """
         for node in Grid.centroide:
-            x=int(node.x*TILESIZE)
-            y=int(node.y*TILESIZE+ largo-1*TILESIZE)
-            #print(str(x)+" "+str(y))
-            pg.draw.circle(screen, RED, (x,y), int(C_cuadros/2)*TILESIZE, 16)
-            #pg.draw.circle(screen, RED, ((node.x * TILESIZE), (node.y * TILESIZE) + largo-1*TILESIZE), 4*TILESIZE, 1)
+            x=int(node.x*TILESIZE+0.5*TILESIZE)
+            y = int(node.y * TILESIZE+0.5*TILESIZE)
+            pg.draw.circle(screen, RED, (x,y), int(C_cuadros/2)*TILESIZE, 0)
 
-    def _draw_ball_(self, Grid, Ball):
+        for node in Grid.P_Malas:
+            x=int(node.x*TILESIZE+0.5*TILESIZE)
+            y = int(node.y * TILESIZE+0.5*TILESIZE)
+            if node != self.Grid.ENEMIGO:
+                pg.draw.circle(screen, BLUE, (x,y), int(C_cuadros/2)*TILESIZE, 0)
+
+        if self.Grid.ICIVA != None:
+            # Vehiculo ICIVA
+            rect = pg.Rect(((self.Grid.ICIVA.x * TILESIZE), (self.Grid.ICIVA.y * TILESIZE)), (TILESIZE, TILESIZE))
+            pg.draw.rect(screen, YELLOW, rect)
+
+        if self.Grid.ENEMIGO != None:
+            # Vehiculo ENEMIGO
+            rect = pg.Rect(((self.Grid.ENEMIGO.x * TILESIZE), (self.Grid.ENEMIGO.y * TILESIZE)), (TILESIZE, TILESIZE))
+            pg.draw.rect(screen, GREEN_APPLE, rect)
+
+    def _draw_ball_(self, Grid, Ball, matrix):
         for node in Grid.centroide:
             #print("node: "+str(node))
             for i in range(C_cuadros):#*TILESIZE):
@@ -124,9 +294,12 @@ class Visualize():
                     #print("j: " + str(j))
                     coord_X = node.x * TILESIZE - int(C_cuadros/2)*TILESIZE + i * TILESIZE
                     coord_Y = node.y * TILESIZE - int(C_cuadros/2)*TILESIZE + j * TILESIZE
+                    #print("Coordenada X: "+str(int(coord_X/TILESIZE)))
+                    #print("Coordenada Y: "+str(int(coord_Y/TILESIZE)))
+                    #self.matrix.itemset((int(coord_Y/TILESIZE), int(coord_X/TILESIZE)), 0.9)
                     Grid.aliveNodes.append((vec(coord_X, coord_Y))// TILESIZE)
                     Ball.relleno.append((vec(coord_X, coord_Y))// TILESIZE)
-
+        #print(matrix)
                     #print("La coordenada es: "+str(coord_X)+", "+str(coord_Y))
         #print("Grid.aliveNodes: ")
         #print(Grid.aliveNodes())
