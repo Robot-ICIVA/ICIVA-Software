c605529e178ff29930740e01ad71a634c4414888
Lujano <lugaluchinchilla@gmail.com>
2018-06-30 22:50:56 -0430

    Comienzo Caracterizacion motores

--- ColorBallTracker/CaracterizacionPlanta/Cliente.py
+++ ColorBallTracker/CaracterizacionPlanta/Cliente.py
@@ -4,10 +4,10 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-from ColorBallTracker.Host2.lib.SerialLib import open_port, close_port, Micro_reset, Micro_comfirm_ACK
+from ColorBallTracker.CaracterizacionPlanta.lib.SerialLib import open_port, close_port, Micro_reset, Micro_comfirm_ACK
 import sys
 import os
-from ColorBallTracker.Host2.lib.MotorLib import align, move_forward, control_w
+from ColorBallTracker.CaracterizacionPlanta.lib.MotorLib import align, move_forward, control_w, send_PWM
 #plt.ion() # activar plot interactivo
 
 def unit_vector(vector):
@@ -92,13 +92,10 @@ def Request(ip, port_server): # el servo que controla phi (plano xy)
 def main():
     port = open_port() # puerto bluetooth
 
-    # Trayectorias
+    # Trayectoria del robot
     tr_robot = np.zeros([0, 2]) # array para guardar x y y del robot
-    tr_obj = np.zeros([0, 2])  # array para guardar x y y de la trayectoria deseada
-    poly = np.poly1d([0.01, 0, 0])
-    x_array = np.arange(0, 100, 2)
-    y_array = poly(x_array)
 
+   # Reseteo de micro
     Micro_reset(port)
     ACK = Micro_comfirm_ACK(port)
     objp = np.zeros((100, 3), np.uint8)
@@ -116,25 +113,17 @@ def main():
     # Vectors
     robot_ini = np.array(
         [x_yellow + (x_cyan - x_yellow) / 2, y_yellow + (y_cyan - y_yellow) / 2])  # posicion del robot
-    # Ajustar la trayectoria
-    for i in range(np.size(x_array)):
-        if x_array[i]<100.0 and y_array[i]<100.0: # limitar trayectoria
-            tr_obj = np.append(tr_obj, [[x_array[i]+robot_ini[0], y_array[i]+robot_ini[1]]], 0)
 
 
-    plt.figure(3)
-    plt.xlim([0, 100])
-    plt.ylim([0, 100])
-    plt.scatter(tr_robot[:, 0], tr_robot[:, 1], c='b')
-    plt.scatter(tr_obj[:, 0], tr_obj[:, 1], c='g')
-    # plt.quiver(robot_pos[0], robot_pos[1], head_vector[0], head_vector[1], color='r')
-    # plt.quiver(robot_pos[0], robot_pos[1], obj[0], obj[1], color='g')
-    plt.waitforbuttonpress(5)
-    obj_index = 2  # indice del objetivo (segundo elemento de la trayectoria)
-    while True:
-        T_Inicio = time.time()
-        obj = tr_obj[obj_index]
-        threshold = 15
+
+    last_pos = robot_ini
+    vector_rapidez_prom = np.array([])
+
+    # Llegar al punto de referencia
+    obj_ini = np.array([15, 50])  # Punto de referencia inicial donde comienza el proceso
+    iciva_arrive = 0
+    while iciva_arrive != 1:
+        threshold = 15 # angular
         response = Request("127.0.0.1", "8000")
         nro_balls = len(response)
         print("Numero de circulos = {}".format(nro_balls))
@@ -147,32 +136,85 @@ def main():
             tr_robot = np.append(tr_robot, [robot_pos], 0)
             # Vectores referenciados a la posicion del robot
             head_vector = np.array([x_cyan - robot_pos[0], y_cyan - robot_pos[1]])
-            ball_vector = np.array([obj[0]-robot_pos[0], obj[1]- robot_pos[1]])
+            ball_vector = obj_ini-robot_pos
+            #ball_vector = np.array([obj[0]-robot_pos[0], obj[1]- robot_pos[1]])
             angle = angle_between(head_vector, ball_vector)
 
             if angle is not None:
-                threshold = 8
-                print(angle)
                 alineado = align(angle, threshold, port)
                 if alineado == 1:
                     dist_obj = norm_vector(ball_vector)
                     if dist_obj is not None:
                         print("Distancia al objetivo = {}".format(dist_obj))
-                        ready = move_forward(dist_obj, 3, 3, port)
-                        if ready == 1:
-                            # plt.figure(2)
-                            # plt.quiver(robot_pos[0], robot_pos[1], head_vector[0], head_vector[1], color='r')
-                            # plt.quiver(robot_pos[0], robot_pos[1], ball_vector[0], ball_vector[1], color='g')
-                            # plt.plot(robot_pos[0], robot_pos[1], marker='+', c='g', label="Odometry")
-                            # plt.plot(obj[0], obj[1], marker='+', c='b', label="Odometry")
-                            # plt.xlim([0, 100])
-                            # plt.ylim([0, 100])
-                            # plt.waitforbuttonpress()
-                            obj_index = obj_index + 1  # alinear al otro objetivo
-                #control_w(angle, 25000, 300, 200, port)
+                        iciva_arrive = move_forward(dist_obj, 3, 3, port)
+
+    # Alinear horizontalmente para comenzar la calibracion
+    ball_vector = np.array([1, 0])
+    angle = angle_between(head_vector, ball_vector)
+    threshold = 5
+    alineado = align(angle, threshold, port)
+    while alineado!=1:
+        response = Request("127.0.0.1", "8000")
+        nro_balls = len(response)
+        print("Numero de circulos = {}".format(nro_balls))
+        if nro_balls == 2:
+            x_cyan, y_cyan, radius_cyan, x_yellow, y_yellow, radius_yellow = circles_robot(response)
+            robot_pos = np.array(
+                [x_yellow + (x_cyan - x_yellow) / 2, y_yellow + (y_cyan - y_yellow) / 2])
+            head_vector = np.array([x_cyan - robot_pos[0], y_cyan - robot_pos[1]])
+            angle = angle_between(head_vector, ball_vector)
+            alineado = align(angle, threshold, port)
+
+    print("El carro se ha alineado")
+    """
+    # Comenzar prueba de calibracion
+
+    # Mover hacia adelanta
+    Dir2 = 0
+    Dir1 = 1
+    PWMrd = 25000
+    PWMri = 25000
+    send_PWM(Dir1, PWMri, Dir2, PWMrd, port)
+
+    # Obtener la posicion del robot
+    response = Request("127.0.0.1", "8000")
+    x_cyan, y_cyan, radius_cyan, x_yellow, y_yellow, radius_yellow = circles_robot(response)
+    # Vectors
+    robot_pos = np.array(
+        [x_yellow + (x_cyan - x_yellow) / 2, y_yellow + (y_cyan - y_yellow) / 2])  # posicion del robot
+
+    # esperar a que pase los 30 cm
+    while robot_pos[0]< 30:
+        # Obtener la posicion del robot
+        response = Request("127.0.0.1", "8000")
+        x_cyan, y_cyan, radius_cyan, x_yellow, y_yellow, radius_yellow = circles_robot(response)
+        robot_pos = np.array(
+            [x_yellow + (x_cyan - x_yellow) / 2, y_yellow + (y_cyan - y_yellow) / 2])  # posicion del robot
+
+    # Medir si va derecho
+    head_vector = np.array([x_cyan - robot_pos[0], y_cyan - robot_pos[1]])
+    ball_vector = np.array([1, 0])
+    angle = angle_between(head_vector, ball_vector)
+    threshold = 10
+    iciva_arrive = 0
+    while np.abs(angle)< threshold: # mientras se considere derecho
+        response = Request("127.0.0.1", "8000")
+        nro_balls = len(response)
+        print("Numero de circulos = {}".format(nro_balls))
+        if nro_balls == 2:
+            x_cyan, y_cyan, radius_cyan, x_yellow, y_yellow, radius_yellow = circles_robot(response)
+            robot_pos = np.array(
+                [x_yellow + (x_cyan - x_yellow) / 2, y_yellow + (y_cyan - y_yellow) / 2])
+            head_vector = np.array([x_cyan - robot_pos[0], y_cyan - robot_pos[1]])
+            angle = angle_between(head_vector, ball_vector)
+            if robot_pos > 70:
+                send_PWM(1, 0, 1, 0, port) # Mandar a detener al robot
+
+    T_Inicio = time.time()
+    """
 
 
-
+    """
         cv2.imshow("Im", objp)
         c = cv2.waitKey(20)
         if c & 0xFF == ord("q"):
@@ -185,8 +227,24 @@ def main():
             #plt.quiver(robot_pos[0], robot_pos[1], obj[0], obj[1], color='g')
             plt.waitforbuttonpress(100)
             break
+
         T_Final = time.time()
         Dif = T_Final - T_Inicio
+        if robot_pos[0] > 20.0:
+            dist_recorrida = norm_vector(last_pos - robot_pos)
+            rapidez = dist_recorrida/Dif
+            vector_rapidez_prom = np.append(vector_rapidez_prom, [rapidez])
+            print("rapidez = {}" .format(rapidez))
+
+        last_pos = robot_pos
+
+        if robot_pos[0] > 70.0:
+            break
         print("Loop time = {}".format(Dif))
+    rapidez_prom = np.mean(vector_rapidez_prom)
+    print("Rapidez promedio = {}".format(rapidez_prom))
+    
+    """
+    close_port(port)
 if __name__ == '__main__':
         main()
